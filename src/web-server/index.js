7b5ee3e05744a8fae065dfeed846d7346902ce09
Kagami <shimura.kyohei7617@gmail.com>
2023-12-17 08:09:12 +0900

    feat: express event driven

--- src/web-server/index.js
+++ src/web-server/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (_, res) => {
-  res.send('Hello World!');
+  res.send('Top Page!');
+});
+
+app.get('/about', (_, res) => {
+  res.send('About Page!');
 });
 
 app.listen(3000);
