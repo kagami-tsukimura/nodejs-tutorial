1033fb324b5468c00e82788aeefe2ff3b177b8ee
Kagami <shimura.kyohei7617@gmail.com>
2023-12-16 23:48:18 +0900

    feat: console error when fs not argument

--- src/fileSystem.js
+++ src/fileSystem.js
@@ -1,5 +1,5 @@
 const { OUTPUT_TXT_PATH, CALL_MESSAGE } = require('./modules/variableDefine');
-const { callLog, writeFile, readFile } = require('./modules/functionDefine');
+const { readFile, writeFile, callError } = require('./modules/functionDefine');
 
 // コマンドライン引数
 const request = process.argv[2];
@@ -8,4 +8,4 @@ request === 'read'
   ? readFile(OUTPUT_TXT_PATH)
   : request === 'write'
   ? writeFile(OUTPUT_TXT_PATH, CALL_MESSAGE)
-  : callLog('Specify [read] or [write] for the command line argument.');
+  : callError('Specify [read] or [write] for the command line argument.');
