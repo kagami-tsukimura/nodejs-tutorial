25c6932c5108506c0ab2a99d17d318c5a09f2c0c
Kagami <shimura.kyohei7617@gmail.com>
2023-12-16 23:29:21 +0900

    comment

--- src/fileSystem.js
+++ src/fileSystem.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const { OUTPUT_TXT_PATH, CALL_MESSAGE, callLog } = require('./modules/define');
 
+// ファイル書き込み
 fs.writeFile(OUTPUT_TXT_PATH, CALL_MESSAGE, () => {
   callLog('Write Done!');
 });
 
+// writeFileを読み込み
 fs.readFile(OUTPUT_TXT_PATH, (_, data) => {
   callLog(data);
   callLog('Read Done!');
